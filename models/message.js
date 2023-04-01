ca37a3c9e7cee7bc205ad2c8b117eab2c9b9f5fe
unknown <anton_kho@msn.com>
2023-04-01 22:58:28 +0800

    added message model

---
+++ models/message.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const messageSchema = new mongoose.Schema(
+  {
+    fromuserid: {
+      type: ObjectId,
+      ref: "user",
+    },
+    touserid: {
+      type: ObjectId,
+      ref: "user",
+    },
+    conetent: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 160,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
