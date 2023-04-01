d371e5e31e798a3c5ef64ca589d0a4b29ede9404
unknown <anton_kho@msn.com>
2023-04-01 22:42:06 +0800

    added auth

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const requireSignin = (req, res, next) => {
+  try {
+    // console.log("Req Headers => ", req.headers);
+    const decoded = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user.role !== 1) {
+      return res.status(401).send("Unauthorized");
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
