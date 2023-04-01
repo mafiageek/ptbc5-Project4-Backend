55a39ec6d024b44d6c58e9a9afda284005583366
unknown <anton_kho@msn.com>
2023-04-01 22:42:38 +0800

    added category, listing and auth

---
+++ models/listing.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const listingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 160,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    userid: {
+      type: ObjectId,
+      ref: "user",
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
+    location: {
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ["Point"], // 'location.type' must be 'Point'
+      },
+      coordinates: {
+        type: [Number],
+      },
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Listing", listingSchema);
