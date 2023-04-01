44ff923a30d49c835333845ec33f6d5978d63a69
unknown <anton_kho@msn.com>
2023-04-01 22:40:26 +0800

    imported multiple routes

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
+import categoryRoutes from "./routes/category.js";
+import listingRoutes from "./routes/listing.js";
 dotenv.config();
 
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB ERROR => ", err));
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", listingRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(process.env);
 });
 
-const port = process.env.PORT || "5000";
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Node server is running on port ${port}`);
